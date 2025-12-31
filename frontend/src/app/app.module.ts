126cbd989199de1f0badd8b756dcf42b2846921b
lucashenrique.info@gmail.com <lucashenrique.info@gmail.com>
2025-12-31 09:54:29 -0300

    Componente criar produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,9 @@ import {MatCardModule} from'@angular/material/card'
 import {MatListModule} from'@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ForDirective } from './directives/for.directive';
+import { RedDirective } from './directives/red.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    ForDirective,
+    RedDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
